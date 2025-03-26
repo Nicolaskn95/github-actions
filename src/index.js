b7d2a88950f895933314561ab74b4bc59ad129da
Nicolaskn95 <nicolasjap@hotmail.com>
2025-03-26 19:59:56 -0300

    feat: implementado rota de login

--- src/index.js
+++ src/index.js
@@ -13,6 +13,10 @@ app.get('/api/test', (req, res) => {
   res.send({ message: 'Endpoint alternativo para teste ba branch developpppp'});
 });
 
+app.get('/api/login', (req, res) => {
+  res.send({ message: 'logado!'});
+});
+
 app.get('/api/test-2', (req, res) => {
   res.send({ message: 'Endpoint de teste na developpppp'});
 });
